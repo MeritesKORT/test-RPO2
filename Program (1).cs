ed4101bd258c40b12efa298d4e92a96281617a2c
AndrewBezotchestvo <03051820dr@gmail.com>
2025-10-30 12:24:44 +0800

    Add currency converter console application
    Introduces a console app that converts rubles to USD, EUR, or JPY based on user input. The program prompts for an amount in rubles and a target currency, then outputs the converted value using fixed exchange rates.

---
+++ Program (1).cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сумму в рублях : ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите во что перевести 1 - $ 2 - € 3 - ¥ : ");
+            int answer = int.Parse(Console.ReadLine());
+
+
+            switch (answer)
+            {
+                case 1:
+                    Console.WriteLine($"{number / 80.70}$");
+
+
+                    break;
+
+                case 2:
+                    Console.WriteLine($"{number  / 94.67}€");
+
+
+
+                    break;
+
+                case 3:
+                    Console.WriteLine($"{number / 11.47}¥");
+
+
+
+                    break;
+
+
+
+            }
+        }
+    }
+}
